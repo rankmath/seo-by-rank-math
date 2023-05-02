e6e03bf0c41343cca4c68cb84c898b4722fc5c7b
Nicholas Babu <nijoba33@gmail.com>
2023-05-02 19:37:41 +0300

    fixed missing nested higher level headers list of list

--- includes/modules/schema/blocks/toc/class-block-toc.php
+++ includes/modules/schema/blocks/toc/class-block-toc.php
@@ -60,16 +60,15 @@ class Block_TOC extends Block {
 			return;
 		}
 
-
 		$this->filter( 'rank_math/schema/block/toc-block', 'add_graph', 10, 2 );
-		//$this->filter( 'render_block_rank-math/toc-block', 'render_toc_block_content', 10, 2 );
+		// $this->filter( 'render_block_rank-math/toc-block', 'render_toc_block_content', 10, 2 );
 		$this->filter( 'rank_math/metabox/post/values', 'block_settings_metadata' );
-		//register_block_type( RANK_MATH_PATH . 'includes/modules/schema/blocks/toc/block.json' );
+		// register_block_type( RANK_MATH_PATH . 'includes/modules/schema/blocks/toc/block.json' );
 		register_block_type(
 			RANK_MATH_PATH . 'includes/modules/schema/blocks/toc/block.json',
-			[
-				'render_callback' => [ $this, 'render'],
-			]
+			array(
+				'render_callback' => array( $this, 'render' ),
+			)
 		);
 
 	}
@@ -83,7 +82,7 @@ class Block_TOC extends Block {
 	 */
 	public function block_settings_metadata( $values ) {
 		$values['tocTitle']           = Helper::get_settings( 'general.toc_block_title' );
-		$values['tocExcludeHeadings'] = Helper::get_settings( 'general.toc_block_exclude_headings', [] );
+		$values['tocExcludeHeadings'] = Helper::get_settings( 'general.toc_block_exclude_headings', array() );
 		$values['listStyle']          = Helper::get_settings( 'general.toc_block_list_style', 'ul' );
 
 		return $values;
@@ -92,8 +91,8 @@ class Block_TOC extends Block {
 	public function render( $attributes ) {
 		// @TODO cache with count($attributes['headings']) maybe
 
-		$headings = $this->linear_to_nested_heading_list($attributes['headings']);
-		$list_out_put = $this->list_output($headings);
+		$headings     = $this->linear_to_nested_heading_list( $attributes['headings'] );
+		$list_out_put = $this->list_output( $headings );
 
 		/**
 		 * @TODO apply settings/options,
@@ -109,23 +108,23 @@ class Block_TOC extends Block {
 		 */
 
 		// Settings
-		$title_wrapper = $attributes['titleWrapper'];
-		$list_style = Helper::get_settings('general.toc_block_list_style') ?? $attributes['listStyle'];
-		$title = ! empty( $attributes['title'] ) ? $attributes['title'] : Helper::get_settings( 'general.toc_block_title' );
-		$exclude_headings = Helper::get_settings('general.toc_block_exclude_headings');
+		$title_wrapper    = $attributes['titleWrapper'];
+		$list_style       = Helper::get_settings( 'general.toc_block_list_style' ) ?? $attributes['listStyle'];
+		$title            = ! empty( $attributes['title'] ) ? $attributes['title'] : Helper::get_settings( 'general.toc_block_title' );
+		$exclude_headings = Helper::get_settings( 'general.toc_block_exclude_headings' );
 
 		$list_tag = self::get()->get_list_style( $list_style );
 		$item_tag = self::get()->get_list_item_style( $list_style );
 
-		//dump($attributes);
+		// dump($attributes);
 
-		$class    = 'rank-math-block';
+		$class = 'rank-math-block';
 		if ( ! empty( $attributes['className'] ) ) {
 			$class .= ' ' . esc_attr( $attributes['className'] );
 		}
 
 		// HTML.
-		$out   = [];
+		$out   = array();
 		$out[] = sprintf(
 			'<div id="rank-math-toc" class="%1$s"%2$s><%3$s>%4$s</%3$s>',
 			$class,
@@ -136,7 +135,7 @@ class Block_TOC extends Block {
 
 		$out[] = '<nav><div>';
 		$out[] = $list_out_put;
-		$out[] ='</div></nav>';
+		$out[] = '</div></nav>';
 		$out[] = '</div>';
 
 		return join( "\n", $out );
@@ -161,14 +160,18 @@ class Block_TOC extends Block {
 			return $block_content;
 		}
 
-		$block_content = preg_replace_callback( '/(<div class=".*?wp-block-rank-math-toc-block.*?"\>)/i', function( $value ) use ( $title, $block_content ) {
-			if ( ! isset( $value[0] ) ) {
-				return $block_content;
-			}
+		$block_content = preg_replace_callback(
+			'/(<div class=".*?wp-block-rank-math-toc-block.*?"\>)/i',
+			function( $value ) use ( $title, $block_content ) {
+				if ( ! isset( $value[0] ) ) {
+					return $block_content;
+				}
 
-			$value[0] = str_replace( '>', ' id="rank-math-toc">', $value[0] );
-			return $value[0] . '<h2>' . esc_html( $title ) . '</h2>';
-		}, $block_content );
+				$value[0] = str_replace( '>', ' id="rank-math-toc">', $value[0] );
+				return $value[0] . '<h2>' . esc_html( $title ) . '</h2>';
+			},
+			$block_content
+		);
 
 		return str_replace( 'class=""', '', $block_content );
 	}
@@ -189,7 +192,7 @@ class Block_TOC extends Block {
 		}
 
 		if ( ! isset( $data['toc'] ) ) {
-			$data['toc'] = [];
+			$data['toc'] = array();
 		}
 
 		foreach ( $attributes['headings'] as $heading ) {
@@ -197,13 +200,13 @@ class Block_TOC extends Block {
 				continue;
 			}
 
-			$data['toc'][] = [
-				"@context" => 'https://schema.org',
-				"@type"    => 'SiteNavigationElement',
-				"@id"      => '#rank-math-toc',
-				"name"     => $heading['content'],
-				"url"      => get_permalink() . $heading['link'],
-			];
+			$data['toc'][] = array(
+				'@context' => 'https://schema.org',
+				'@type'    => 'SiteNavigationElement',
+				'@id'      => '#rank-math-toc',
+				'name'     => $heading['content'],
+				'url'      => get_permalink() . $heading['link'],
+			);
 		}
 
 		if ( empty( $data['toc'] ) ) {
@@ -216,76 +219,75 @@ class Block_TOC extends Block {
 
 	/**
 	 * Nest heading based on the Heading level.
+	 *
 	 * @param array $heading_list The flat list of headings to nest.
 	 *
 	 * @return array The nested list of headings.
 	 */
 	private function linear_to_nested_heading_list( $heading_list ) {
-		$nexted_heading_list = [];
-		//dump($heading_list);
-		foreach ($heading_list as $key => $heading) {
-			if (empty($heading['content'])) {
+		$nexted_heading_list = array();
+		// dump($heading_list);
+		foreach ( $heading_list as $key => $heading ) {
+			if ( empty( $heading['content'] ) ) {
 				continue;
 			}
 
 			// Make sure we are only working with the same level as the first iteration in our set.
-			if (  $heading['level'] === $heading_list[ 0 ]['level'] ){
+			if ( $heading['level'] === $heading_list[0]['level'] ) {
 
 				// Propagate to children only (those whose level is lower than their parent ie $heading['level'].
-                // @TODO.. don't pass on higher level headers below lower, this causes mulitple values of the same!!!
-                // @TODO slice upto where we have the higher level because that's where the children end!
-				if ( isset($heading_list[ $key + 1]['level']) &&  $heading_list[ $key + 1]['level'] > $heading['level']) {
+				// @TODO.. don't pass on higher level headers below lower, this causes mulitple values of the same!!!
+				// @TODO slice upto where we have the higher level because that's where the children end!
+				if ( isset( $heading_list[ $key + 1 ]['level'] ) && $heading_list[ $key + 1 ]['level'] > $heading['level'] ) {
 
 					// endOfSlice should be upto where heading level is smaller than then current
-					$endOfSlice = $this->get_end_of_slice($heading_list);
-					$children = $this->linear_to_nested_heading_list(
-						array_slice($heading_list,  $key + 1, $endOfSlice)
+					$endOfSlice = $this->get_end_of_slice( $heading_list );
+					$children   = $this->linear_to_nested_heading_list(
+						array_slice( $heading_list, $key + 1, $endOfSlice )
 					);
 
 					array_push(
 						$nexted_heading_list,
-						[
-							'item' => $heading,
-							'children' => $children
-						]
+						array(
+							'item'     => $heading,
+							'children' => $children,
+						)
 					);
 				} else {
 					array_push(
 						$nexted_heading_list,
-						[
-							'item' => $heading,
-							'children' => null
-						]
+						array(
+							'item'     => $heading,
+							'children' => null,
+						)
 					);
 				}
+			} elseif ( $heading['level'] < $heading_list[0]['level'] ) {
+				$endOfSlice  = count( $heading_list );
+				$items_array = array_slice( $heading_list, $key, $endOfSlice );
+				$items       = $this->linear_to_nested_heading_list( $items_array );
 
-			} elseif ( $heading['level'] < $heading_list[ 0 ]['level'] && $heading_list[ $key + 1]['level'] > $heading['level'] ) {
-				$endOfSlice = count($heading_list);
-				$items_array = array_slice($heading_list,  $key, $endOfSlice);
-				$items = $this->linear_to_nested_heading_list( $items_array	);
-
-				array_push($nexted_heading_list, $items[0]);
+				array_push( $nexted_heading_list, $items[0] );
 
 			}
-
 		}
 
 		return $nexted_heading_list;
 
 	}
 
-	private function list_output( $headings ){
+	private function list_output( $headings ) {
 		$out[] = '<ul>';
 
-		foreach ($headings as $heading) {
+		foreach ( $headings as $heading ) {
 			$out[] = sprintf(
 				'<div><a href="%1$s">%2$s</a></div>',
 				$heading['item']['link'],
 				$heading['item']['content']
 			);
 
-			if ($heading['children']) {
-				$out[] = $this->list_output($heading['children']);
+			if ( $heading['children'] ) {
+				$out[] = $this->list_output( $heading['children'] );
 			}
 		}
 
@@ -294,16 +296,15 @@ class Block_TOC extends Block {
 
 	}
 
-	private function get_end_of_slice($list) {
+	private function get_end_of_slice( $list ) {
 
-		foreach ($list as $key => $item) {
+		foreach ( $list as $key => $item ) {
 
-			if ($list[0]['level'] > $item['level']) {
+			if ( $list[0]['level'] > $item['level'] && 0 !== $key ) {
 				return $key - 1;
 			}
-
 		}
 
-		return count($list);
+		return count( $list );
 	}
 }
