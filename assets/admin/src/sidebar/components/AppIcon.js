072be2e8d3968b63b79f3ceb8e82f0c8d9129a0e
arunkumar-raja <136444471+arunkumar-raja@users.noreply.github.com>
2023-07-24 18:49:47 +0530

    Fix issues in rebase:
    to check rebase

--- assets/admin/src/sidebar/components/AppIcon.js
+++ assets/admin/src/sidebar/components/AppIcon.js
@@ -6,22 +6,21 @@ import classnames from 'classnames'
 /**
  * WordPress dependencies
  */
-import { withSelect } from '@wordpress/data'
-
+import { withSelect, dispatch } from '@wordpress/data'
 /**
  * Internal dependencies
  */
 import AnalysisScore from '@components/AnalysisScore'
 import getClassByScore from '@helpers/getClassByScore'
 
-const RankMathIcon = ( { isLoaded, score } ) => {
+const RankMathIcon = ( { isLoaded, score, toggleEditor } ) => {
 	const classes = classnames( 'rank-math-toolbar-score', {
 		[ getClassByScore( score ) ]: true,
 		loading: ! isLoaded,
 	} )
 
 	return (
-		<div className={ classes }>
+		<div className={ classes } onClick={ toggleEditor }>
 			<i className="rm-icon rm-icon-rank-math"></i>
 			<AnalysisScore />
 		</div>
@@ -34,5 +33,8 @@ export default withSelect( ( select ) => {
 	return {
 		isLoaded: repo.isLoaded(),
 		score: repo.getAnalysisScore(),
+		toggleEditor() {
+			dispatch( 'core/editor' ).editPost({meta:{rankmath:'saving_post'}})
+		},
 	}
 } )( RankMathIcon )
