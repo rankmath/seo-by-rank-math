96ff6c0aeb79921ad2828dbb851c4b2d2775175e
Francis <numofran6@gmail.com>
2023-08-12 10:19:55 +0000

    button: secondary, secondary-grey and tertiary variant

--- includes/modules/components/assets/src/buttons/Button.js
+++ includes/modules/components/assets/src/buttons/Button.js
@@ -19,14 +19,14 @@ const Button = ({ children, variant = 'primary', size = 'default', disabled = fa
     size === 'large' && classes.push('is-large');
 
     variant === 'primary-outline' && classes.push('primary-outline');
-
+    variant === 'secondary-grey' && classes.push('secondary-grey');
 
     return classes.join(' ')
   }
 
   return (
     <WPButton
-      variant={variant === 'primary-outline' ? 'secondary' : variant}
+      variant={(variant === 'primary-outline' || variant === 'secondary-grey' || variant === 'tertiary') ? 'secondary' : variant}
       size={size}
       disabled={disabled}
       aria-disabled={disabled}
--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -14,7 +14,7 @@ const AllComponents = () => {
     <Fragment>
       <h4>Text Buttons</h4>
 
-      <div className='group-section'>
+      <div>
         <h3>Primary</h3>
 
         <div className='group-button'>
@@ -25,7 +25,7 @@ const AllComponents = () => {
         </div>
       </div>
 
-      <div className='group-section'>
+      <div>
         <h3>Primary Outline</h3>
 
         <div className='group-button'>
@@ -33,6 +33,39 @@ const AllComponents = () => {
           <Button variant='primary-outline'>Label</Button>
           <Button variant='primary-outline' size='large'>Label</Button>
           <Button variant='primary-outline' size='large' disabled>Label</Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Secondary</h3>
+
+        <div className='group-button'>
+          <Button variant='secondary' size='small'>Label</Button>
+          <Button variant='secondary'>Label</Button>
+          <Button variant='secondary' size='large'>Label</Button>
+          <Button variant='secondary' size='large' disabled>Label</Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Secondary Grey</h3>
+
+        <div className='group-button'>
+          <Button variant='secondary-grey' size='small'>Label</Button>
+          <Button variant='secondary-grey'>Label</Button>
+          <Button variant='secondary-grey' size='large'>Label</Button>
+          <Button variant='secondary-grey' size='large' disabled>Label</Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Tertiary</h3>
+
+        <div className='group-button'>
+          <Button variant='tertiary' size='small'>Label</Button>
+          <Button variant='tertiary'>Label</Button>
+          <Button variant='tertiary' size='large'>Label</Button>
+          <Button variant='tertiary' size='large' disabled>Label</Button>
         </div>
       </div>
     </Fragment>
