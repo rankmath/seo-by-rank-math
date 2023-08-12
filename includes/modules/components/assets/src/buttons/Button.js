e1a20cc82eaf5369bdcb825319d70e6a1fe92d63
Francis <numofran6@gmail.com>
2023-08-12 08:40:09 +0000

    button: primary outline variant

--- includes/modules/components/assets/src/buttons/Button.js
+++ includes/modules/components/assets/src/buttons/Button.js
@@ -12,20 +12,25 @@ import '../../scss/Button.scss'
  */
 import { Button as WPButton } from '@wordpress/components';
 
-const Button = ({ children, variant = 'primary', size, disabled = false }) => {
-  let buttonClass = ''
+const Button = ({ children, variant = 'primary', size = 'default', disabled = false }) => {
+  const getButtonClasses = () => {
+    const classes = []
 
-  if (size === 'large') {
-    buttonClass += ' is-large'
+    size === 'large' && classes.push('is-large');
+
+    variant === 'primary-outline' && classes.push('primary-outline');
+
+
+    return classes.join(' ')
   }
 
   return (
     <WPButton
-      variant={variant}
+      variant={variant === 'primary-outline' ? 'secondary' : variant}
       size={size}
       disabled={disabled}
       aria-disabled={disabled}
-      className={buttonClass}
+      className={getButtonClasses()}
     >
       {children}
     </WPButton>
--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -14,11 +14,26 @@ const AllComponents = () => {
     <Fragment>
       <h4>Text Buttons</h4>
 
-      <div className='group-button'>
-        <Button size='small'>Label</Button>
-        <Button>Label</Button>
-        <Button size='large'>Label</Button>
-        <Button size='large' disabled>Label</Button>
+      <div className='group-section'>
+        <h3>Primary</h3>
+
+        <div className='group-button'>
+          <Button size='small'>Label</Button>
+          <Button>Label</Button>
+          <Button size='large'>Label</Button>
+          <Button size='large' disabled>Label</Button>
+        </div>
+      </div>
+
+      <div className='group-section'>
+        <h3>Primary Outline</h3>
+
+        <div className='group-button'>
+          <Button variant='primary-outline' size='small'>Label</Button>
+          <Button variant='primary-outline'>Label</Button>
+          <Button variant='primary-outline' size='large'>Label</Button>
+          <Button variant='primary-outline' size='large' disabled>Label</Button>
+        </div>
       </div>
     </Fragment>
   );
