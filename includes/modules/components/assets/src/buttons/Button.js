6bb28a2e39b26c3c854215b45d9a8343762beb1c
Francis <numofran6@gmail.com>
2023-08-14 08:40:28 +0000

    button: alias removed

--- includes/modules/components/assets/src/buttons/Button.js
+++ includes/modules/components/assets/src/buttons/Button.js
@@ -1,8 +1,4 @@
 /**
- * External dependencies
- */
-
-/**
  * Internal dependencies
  */
 import '../../scss/Button.scss'
@@ -10,9 +6,9 @@ import '../../scss/Button.scss'
 /**
  * WordPress dependencies
  */
-import { Button as WPButton } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 
-export default function Button({
+export default function ({
   children,
   variant = 'primary',
   size = 'default',
@@ -36,7 +32,7 @@ export default function Button({
   const variantClassMap = {
     'primary-outline': 'primary-outline',
     'secondary-grey': 'secondary-grey',
-    tertiary: 'secondary'
+    'tertiary-outline': 'secondary',
   };
 
   const getButtonClasses = () => {
@@ -47,13 +43,15 @@ export default function Button({
 
     if (variantClassMap[variant]) {
       classes += ` ${variantClassMap[variant]}`;
+    } else if (variant === 'tertiary') {
+      classes += 'tertiary'
     }
 
     return classes;
   };
 
   return (
-    <WPButton
+    <Button
       variant={variantClassMap[variant] ? 'secondary' : variant}
       aria-disabled={disabled}
       className={getButtonClasses()}
@@ -79,77 +77,9 @@ export default function Button({
       }}
     >
       {children}
-    </WPButton>
+    </Button>
   )
 }
 
-
-export const ButtonShowcase = () => {
-  return (
-    <div>
-      <h4>Text Buttons</h4>
-
-      <div>
-        <h3>Primary</h3>
-
-        <div className='group-button'>
-          <Button size='small'>Label</Button>
-          <Button>Label</Button>
-          <Button size='large' children={'Label'}></Button>
-
-          <Button size='large' disabled>Label</Button>
-        </div>
-      </div>
-
-      <div>
-        <h3>Primary Outline</h3>
-
-        <div className='group-button'>
-          <Button variant='primary-outline' size='small'>Label</Button>
-          <Button variant='primary-outline'>Label</Button>
-          <Button variant='primary-outline' size='large'>Label</Button>
-
-          <Button variant='primary-outline' size='large' disabled>Label</Button>
-        </div>
-      </div>
-
-      <div>
-        <h3>Secondary</h3>
-
-        <div className='group-button'>
-          <Button variant='secondary' size='small'>Label</Button>
-          <Button variant='secondary'>Label</Button>
-          <Button variant='secondary' size='large'>Label</Button>
-
-          <Button variant='secondary' size='large' disabled>Label</Button>
-        </div>
-      </div>
-
-      <div>
-        <h3>Secondary Grey</h3>
-
-        <div className='group-button'>
-          <Button variant='secondary-grey' size='small'>Label</Button>
-          <Button variant='secondary-grey'>Label</Button>
-          <Button variant='secondary-grey' size='large'>Label</Button>
-
-          <Button variant='secondary-grey' size='large' disabled>Label</Button>
-        </div>
-      </div>
-
-      <div>
-        <h3>Tertiary</h3>
-
-        <div className='group-button'>
-          <Button variant='tertiary' size='small'>Label</Button>
-          <Button variant='tertiary'>Label</Button>
-          <Button variant='tertiary' size='large'>Label</Button>
-
-          <Button variant='tertiary' size='large' disabled>Label</Button>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 // icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}
--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -1,12 +1,12 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
 import { createElement, render } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { ButtonShowcase } from './buttons/Button';
+import Button from './buttons/Button';
 import '../scss/components.scss'
 
 const AllComponents = () => {
@@ -16,6 +16,114 @@ const AllComponents = () => {
     </>
   );
 };
+
+function ButtonShowcase() {
+  return (
+    <div>
+      <h4>Text Buttons</h4>
+
+      {/* <div>
+        <h3>Primary</h3>
+
+        <div className='group-button'>
+          <Button
+            size='small'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}
+          ></Button>
+          <Button
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+          <Button
+            size='large'
+            // children={'Label'}
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>} />
+
+          <Button size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>} disabled></Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Primary Outline</h3>
+
+        <div className='group-button'>
+          <Button
+            variant='primary-outline'
+            size='small'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}
+          ></Button>
+          <Button
+            variant='primary-outline'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+          <Button
+            variant='primary-outline'
+            size='large'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}
+          ></Button>
+
+          <Button variant='primary-outline' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>} disabled></Button>
+        </div>
+      </div> */}
+
+      <div>
+        <h3>Secondary</h3>
+
+        <div className='group-button'>
+          <Button
+            variant='secondary'
+            size='small'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}
+          ></Button>
+          <Button
+            variant='secondary'
+            icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+          <Button variant='secondary' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='secondary' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>} disabled></Button>
+        </div>
+      </div>
+
+      {/* <div>
+        <h3>Secondary Grey</h3>
+
+        <div className='group-button'>
+          <Button variant='secondary-grey' size='small' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+          <Button
+            variant='secondary-grey' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+          <Button variant='secondary-grey' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='secondary-grey' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>} disabled></Button>
+        </div>
+      </div> */}
+
+      <div>
+        <h3>Tertiary Outline</h3>
+
+        <div className='group-button'>
+          <Button variant='tertiary-outline' size='small' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary-outline' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary-outline' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary-outline' size='large' disabled icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+        </div>
+      </div>
+
+      <div>
+        <h3>Tertiary</h3>
+
+        <div className='group-button'>
+          <Button variant='tertiary' size='small' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary' size='large' icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+
+          <Button variant='tertiary' size='large' disabled icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M10 17.389H8.444A5.194 5.194 0 1 1 8.444 7H10v1.5H8.444a3.694 3.694 0 0 0 0 7.389H10v1.5ZM14 7h1.556a5.194 5.194 0 0 1 0 10.39H14v-1.5h1.556a3.694 3.694 0 0 0 0-7.39H14V7Zm-4.5 6h5v-1.5h-5V13Z"></path></svg>}></Button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 const renderAllComponents = () => {
   const componentsUi = document.getElementById('components-page');
