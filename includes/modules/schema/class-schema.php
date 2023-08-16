298e94255cc99bfd72535f3e095fbb4a487055f5
arunkumar-raja <136444471+arunkumar-raja@users.noreply.github.com>
2023-08-16 18:16:34 +0530

    To set oembed title to fix linked in issue

--- includes/modules/schema/class-schema.php
+++ includes/modules/schema/class-schema.php
@@ -11,7 +11,7 @@
 namespace RankMath\Schema;
 
 use RankMath\Traits\Hooker;
-
+use RankMath\Helper;
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -31,10 +31,32 @@ class Schema {
 		}
 		$this->action( 'wp', 'integrations' );
 		$this->filter( 'rank_math/elementor/dark_styles', 'add_dark_style' );
-
+		$this->filter( 'oembed_response_data', 'set_oembed_data', 10, 2 );
 		new Blocks();
 		new Snippet_Shortcode();
 	}
+
+	/**
+	 * Callback function to pass to the oEmbed's response data that will enable
+	 * support for using the image and title set by the WordPress SEO plugin's fields. This
+	 * address the concern where some social channels/subscribed use oEmebed data over Open Graph data
+	 * if both are present.
+	 *
+	 * @link https://developer.wordpress.org/reference/hooks/oembed_response_data/ for hook info.
+	 *
+	 * @param array   $data The oEmbed data.
+	 * @param WP_Post $post The current Post object.
+	 *
+	 * @return array An array of oEmbed data with modified values where appropriate.
+	 */
+	public function set_oembed_data( $data, $post ) {
+		$title = get_post_meta( $post->ID, 'rank_math_title', true );
+		if ( ! empty( $title ) ) {
+			$data['title'] = Helper::replace_vars( $title, $post );
+		}
+		return $data;
+	}
+
 
 	/**
 	 * Add dark style
