5fbaca6cbe6bb3f75fa1e9306b0bfca9f45ed586
Francis <numofran6@gmail.com>
2023-08-19 16:20:47 +0000

    PageTabPanel: fully implemented

--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -8,66 +8,29 @@ import { createElement, render, useState } from '@wordpress/element';
 */
 import '../../../../../assets/admin/scss/_font-icons.scss';
 import '../scss/components.scss';
-import SidebarTabPanel from './tabs/SidebarTabPanel';
 import TabPanelWithIcon from './tabs/TabPanelWithIcon';
+import PageTabPanel from './tabs/PageTabPanel';
 
 
 const AllComponents = () => {
-
   return (
     <div className='components-container'>
-      <SidebarTabPanel
+      <PageTabPanel
         tabs={[
           {
             name: 'tab1',
-            title: 'First',
-            icon: <TabPanelWithIcon icon='rm-icon-trash' title='First' />
+            icon: <TabPanelWithIcon icon='rm-icon-trash' title='First Tab' />
           },
           {
             name: 'tab2',
-            title: 'Second',
-            icon: <TabPanelWithIcon icon='rm-icon-trash' title='Second' />
+            icon: <TabPanelWithIcon icon='rm-icon-trash' title='Second Tab' />
           },
           {
             name: 'tab3',
-            title: 'Third',
-            icon: <TabPanelWithIcon icon='rm-icon-trash' title='Third' />
+            icon: <TabPanelWithIcon icon='rm-icon-trash' title='Third Tab' />
           }
         ]}
-        children={(tab) => (
-          <div>
-            <h3>{tab.title}</h3>
-            <p>This is the content for tab {tab.name}</p>
-          </div>
-        )
-        }
       />
-
-      <div style={{ marginTop: '1rem' }}>
-        <SidebarTabPanel
-          tabs={[
-            {
-              name: 'tab1',
-              title: 'First Tab',
-            },
-            {
-              name: 'tab2',
-              title: 'Second Tab',
-            },
-            {
-              name: 'tab3',
-              title: 'Third Tab',
-            }
-          ]}
-          children={(tab) => (
-            <div>
-              <h3>{tab.title}</h3>
-              <p>This is the content for tab {tab.name}</p>
-            </div>
-          )
-          }
-        />
-      </div>
     </div>
   )
 };
