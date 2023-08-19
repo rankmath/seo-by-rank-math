aea985213609114550ea50b8b0c98ff84e97b7fb
Francis <numofran6@gmail.com>
2023-08-19 12:34:09 +0000

    Notice: completely implemented

--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -6,49 +6,14 @@ import { createElement, render, useState } from '@wordpress/element';
 /**
  * Internal dependencies
 */
-import SearchSelectControl from './select/search-select/SearchSelectControl';
 import '../../../../../assets/admin/scss/_font-icons.scss';
 import '../scss/components.scss';
 
 
 const AllComponents = () => {
-  const optionsList = [
-    {
-      key: 'first_option',
-      name: {
-        title: 'First Option Title',
-        subTitle: '%code_text%',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A magnam, nulla optio doloremque non, debitis aliquid dolores ad, nobis natus porro fugit sint qui amet corporis ipsum? Nam, adipisci iste!'
-      }
-    },
-    {
-      key: 'second_option',
-      name: {
-        title: 'Second Option Title',
-        subTitle: '%code_text%',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A magnam, nulla optio doloremque non, debitis aliquid dolores ad, nobis natus porro fugit sint qui amet corporis ipsum? Nam, adipisci iste!'
-      }
-    },
-  ]
-
-  const [value, setValue] = useState({
-    key: optionsList[0].key,
-    name: optionsList[0].name.title,
-  });
-
   return (
     <div className='components-container'>
-      <div>
-        <SearchSelectControl
-          label="Search Options"
-          value={value}
-          options={optionsList}
-          onChange={
-            ({ selectedItem: { key, name } }) => setValue({ key, name: name.props.title })
-          }
-        // disabled
-        />
-      </div>
+
     </div>
   )
 };
---
+++ includes/modules/components/assets/src/prompts/Notice.js
@@ -0,0 +1,75 @@
+/**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import '../../scss/Notice.scss';
+
+/**
+ * WordPress dependencies
+ */
+import { Notice } from '@wordpress/components';
+
+export default function ({
+  status = 'info',
+  actions = [],
+  isDismissible = true,
+  politeness = 'polite',
+  children,
+  className,
+  spokenMessage,
+  onRemove,
+  icon,
+  ...rest
+}) {
+  const statusToTitle = {
+    error: 'Error!',
+    success: 'Success!',
+    warning: 'Warning!',
+  };
+
+  const title = statusToTitle[status] || 'Notice!';
+
+  const getNoticeClasses = () => {
+    return classNames(
+      {
+        'has-icon': icon,
+      }
+    );
+  };
+
+  const noticeProps = {
+    className: getNoticeClasses(),
+    status,
+    actions,
+    isDismissible,
+    politeness,
+    spokenMessage,
+    onRemove,
+    icon,
+    ...rest
+  }
+
+  return (
+    <Notice {...noticeProps} >
+      {icon &&
+        <div className='components-notice__icon'>
+          {icon}
+        </div>
+      }
+
+      <div className='components-notice__text'>
+        {!icon &&
+          <b className='components-notice__title'>{title}</b>
+        }
+
+        <>
+          {children}
+        </>
+      </div>
+    </Notice>
+  )
+}
