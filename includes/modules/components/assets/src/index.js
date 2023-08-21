5a2e967c4cc54479dc9b473ee024632d20a65168
Francis <numofran6@gmail.com>
2023-08-21 09:48:25 +0000

    MultiSectionTabPanel: fully implemented

--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -9,12 +9,37 @@ import { createElement, render, useState } from '@wordpress/element';
 import '../../../../../assets/admin/scss/_font-icons.scss';
 import '../scss/components.scss';
 import SingleSectionTabPanel from './tabs/vertical-tabs/SingleSectionTabPanel';
+import MultiSectionTabPanel from './tabs/vertical-tabs/MultiSectionTabPanel';
 
 
 const AllComponents = () => {
   return (
     <div className='components-container'>
+      <MultiSectionTabPanel>
+        <SingleSectionTabPanel
+          menuItems={[
+            { title: 'Frist Tab', icon: 'rm-icon-trash' },
+            { title: 'Second Tab', icon: 'rm-icon-trash' },
+            { title: 'Third Tab', icon: 'rm-icon-trash' },
+            { title: 'Fourth Tab', icon: 'rm-icon-trash' },
+          ]}
+        />
+
+        <SingleSectionTabPanel
+          label='Settings'
+          menuItems={[
+            { title: 'Settings 1', icon: 'rm-icon-trash' },
+            { title: 'Settings 2', icon: 'rm-icon-trash' },
+            { title: 'Settings 3', icon: 'rm-icon-trash' },
+            { title: 'Settings 4', icon: 'rm-icon-trash' },
+          ]}
+        />
+      </MultiSectionTabPanel>
+
+      <div style={{ margin: '1rem' }} />
+
       <SingleSectionTabPanel
+        label='Title'
         menuItems={[
           { title: 'Frist Tab', icon: 'rm-icon-trash' },
           { title: 'Second Tab', icon: 'rm-icon-trash' },
@@ -22,8 +47,6 @@ const AllComponents = () => {
           { title: 'Fourth Tab', icon: 'rm-icon-trash' },
         ]}
       />
-
-      {/* <div style={{ margin: '1rem' }} /> */}
     </div>
   )
 };
---
+++ includes/modules/components/assets/src/tabs/vertical-tabs/MultiSectionTabPanel.js
@@ -0,0 +1,15 @@
+/**
+ * Internal dependencies
+ */
+import '../../../scss/MultiSectionTabPanel.scss';
+
+export default function ({
+  children
+}) {
+
+  return (
+    <div className='multi-section-tab-panel'>
+      {children}
+    </div>
+  )
+};
