ebfa8e839a5b2ab30dd768d90a955db6548df3aa
Francis <numofran6@gmail.com>
2023-08-22 16:44:07 +0000

    KeywordSuggestion: tooltip implementation

--- includes/modules/components/assets/src/buttons/KeywordSuggestion.js
+++ includes/modules/components/assets/src/buttons/KeywordSuggestion.js
@@ -11,7 +11,7 @@ import '../../scss/keyword-suggestion.scss'
 /**
  * WordPress dependencies
  */
-import { Button } from '@wordpress/components';
+import { Button, Popover } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
 export default function ({
@@ -21,37 +21,14 @@ export default function ({
   className,
   ...rest
 }) {
-  const [isCopied, setIsCopied] = useState(true);
+  const [isCopied, setIsCopied] = useState(false);
 
   const getButtonClasses = () => {
-    return classNames(
-      'keyword-suggestion-button',
-      className,
-      severity,
-    );
+    return classNames(className, severity,);
   };
 
-  const keywordElement = <h1 className='keyword-suggestion-button__keyword'>{keyword}</h1>;
-  const scoreElement = <h6 className='keyword-suggestion-button__score'>{score}</h6>;
-
   const handleCopyClick = () => {
-    if (!isCopied) {
-      const combinedText = `${keyword} ${score}`;
-
-      if (navigator.clipboard) {
-        navigator.clipboard.writeText(combinedText)
-          .catch(err => {
-            console.error('Failed to copy text:', err);
-          });
-      } else {
-        const tempElement = document.createElement('textarea');
-        tempElement.value = combinedText;
-        document.body.appendChild(tempElement);
-        tempElement.select();
-        document.execCommand('copy');
-        document.body.removeChild(tempElement);
-      }
-    }
+    setIsCopied(true);
   };
 
   const buttonProps = {
@@ -59,24 +36,25 @@ export default function ({
     className: getButtonClasses(),
     onClick: handleCopyClick,
     variant: 'secondary',
-    children: (
-      <>
-        {keywordElement}
-        {scoreElement}
-      </>
-    ),
-    ...(isCopied
-      ? {
-        label: 'Copied!',
-        showTooltip: true,
-        tooltipPosition: "top center",
-      }
-      : {})
   }
 
   return (
-    <Button
-      {...buttonProps}
-    />
+    <div className='keyword-suggestion-button'>
+      <Button {...buttonProps}>
+        <h1 className='keyword-suggestion-button__keyword'>{keyword}</h1>
+        <h6 className='keyword-suggestion-button__score'>{score}</h6>
+      </Button>
+
+      {isCopied && (
+        <Popover
+          placement="top"
+          onClose={() => setIsCopied(false)}
+          noArrow={false}
+          offset={11}
+        >
+          Copied!
+        </Popover>
+      )}
+    </div>
   )
 };
