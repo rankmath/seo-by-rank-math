56000ddc53a12ba09dca5b27ec6eb2e064279ed9
Francis <numofran6@gmail.com>
2023-08-23 15:33:25 +0000

    EditorScoreBar: fully implemented

--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -8,78 +8,21 @@ import { createElement, render, useState } from '@wordpress/element';
 */
 import '../../../../../assets/admin/scss/_font-icons.scss';
 import '../scss/components.scss';
-import AnchorTagStatus from './buttons/AnchorTagStatus';
-import ScoreButton from './buttons/ScoreButton';
-import KeywordButton from './buttons/KeywordButton';
-import ConnectionButton from './buttons/ConnectionButton';
+import EditorScoreBar from './score-bars/EditorScoreBar';
 
 
 const AllComponents = () => {
   return (
     <div className='components-container'>
-      <div>
-        <h3>Connection Button</h3>
 
-        <div className='group-components'>
-          <ConnectionButton status='connect'>Connect</ConnectionButton>
-          <ConnectionButton status='connected'>Connected</ConnectionButton>
-        </div>
-
-        <div className='group-components'>
-          <ConnectionButton status='disconnect'>Disconnect</ConnectionButton>
-          <ConnectionButton status='disconnected'>Disconnected</ConnectionButton>
-        </div>
-
-        <div className='group-components'>
-          <ConnectionButton status='reconnect'>Reconnect</ConnectionButton>
-          <ConnectionButton status='connected'>Connected</ConnectionButton>
-        </div>
+      <div className="group-components">
+        <EditorScoreBar value={23} />
       </div>
-
-      <div>
-        <h3>Anchor Tag Status</h3>
-
-        <div className='group-components'>
-          <AnchorTagStatus>All Good</AnchorTagStatus>
-
-          <AnchorTagStatus severity='warning' children='Warning' />
-
-          <AnchorTagStatus severity='error' children='Error' />
-
-          <AnchorTagStatus severity='grey' children='Label' />
-        </div>
+      <div className="group-components">
+        <EditorScoreBar value={52} />
       </div>
-
-      <div>
-        <h3>Score Button</h3>
-
-        <div className='group-components'>
-          <ScoreButton>94/100</ScoreButton>
-
-          <ScoreButton severity='neutral'>52/100</ScoreButton>
-
-          <ScoreButton severity='bad'>52/100</ScoreButton>
-        </div>
-
-        <div className='group-components'>
-          <ScoreButton company='Content AI'>94/100</ScoreButton>
-
-          <ScoreButton company='Content AI' severity='neutral'>52/100</ScoreButton>
-
-          <ScoreButton company='Content AI' severity='bad'>52/100</ScoreButton>
-        </div>
-      </div>
-
-      <div>
-        <h3>Keyword Button</h3>
-
-        <div className='group-components'>
-          <KeywordButton />
-
-          <KeywordButton keyword='Increase' severity='neutral' />
-
-          <KeywordButton severity='bad' />
-        </div>
+      <div className="group-components">
+        <EditorScoreBar value={94} />
       </div>
 
     </div>
---
+++ includes/modules/components/assets/src/score-bars/EditorScoreBar.js
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import '../../scss/editor-score-bar.scss';
+
+/**
+ * WordPress dependencies
+ */
+import { RangeControl } from '@wordpress/components';
+
+export default function ({
+  value,
+  className,
+  ...rest
+}) {
+  const getButtonClasses = () => {
+    return classNames(
+      'editor-score-bar',
+      className,
+      {
+        'bad-score': value < 50,
+        'average-score': value > 50 && value <= 70,
+        'good-score': value > 70
+      }
+    );
+  };
+
+  const rangeControlProps = {
+    ...rest,
+    value,
+    className: getButtonClasses(),
+    min: 0,
+    max: 100,
+    withInputField: false,
+    showTooltip: false,
+    label: `${value} / 100`
+  }
+
+  return (
+    <RangeControl
+      {...rangeControlProps}
+    />
+  );
+}
