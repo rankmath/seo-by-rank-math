86958b6a50e184a577534ce7b6b88ab12153cb09
Francis <numofran6@gmail.com>
2023-08-25 07:18:33 +0000

    ImageUploader: file structure

--- includes/modules/components/assets/src/file-uploaders/ImageUploader.js
+++ includes/modules/components/assets/src/file-uploaders/ImageUploader.js
@@ -11,13 +11,17 @@ import '../../scss/image-uploader.scss'
 /**
  * WordPress dependencies
  */
-import { RangeControl, Button } from '@wordpress/components';
+import { RangeControl, FormFileUpload } from '@wordpress/components';
 
 export default function () {
   return (
     <div className='image-uploader-container'>
       <div>
-        <Button>Add or Upload Image</Button>
+        <FormFileUpload
+          accept="image/*"
+        >
+          Add or Upload Image
+        </FormFileUpload>
 
         <span>Min: 1400x1400px. &bull; Max: 3000x3000px. &bull; Max file size: 0.5MB.</span>
       </div>
