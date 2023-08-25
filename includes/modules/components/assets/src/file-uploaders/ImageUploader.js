9d464b8c66103a06cbf4b8c029dc4bec3a0bf569
Francis <numofran6@gmail.com>
2023-08-25 08:42:35 +0000

    ImageUploader: variant 1

--- includes/modules/components/assets/src/file-uploaders/ImageUploader.js
+++ includes/modules/components/assets/src/file-uploaders/ImageUploader.js
@@ -15,18 +15,21 @@ import { RangeControl, FormFileUpload } from '@wordpress/components';
 
 export default function () {
   return (
-    <div className='image-uploader-container'>
-      <div>
+    <div className='image-uploader'>
+      <div className='image-uploader__controls'>
         <FormFileUpload
           accept="image/*"
         >
-          Add or Upload Image
+          <i className='rm-icon-export'></i> <span>Add or Upload Image</span>
         </FormFileUpload>
 
         <span>Min: 1400x1400px. &bull; Max: 3000x3000px. &bull; Max file size: 0.5MB.</span>
       </div>
 
-      <img src="" alt="" />
+      <div className='image-uploader__preview'>
+        <span className='image-uploader__preview-title'>Image Preview</span>
+        {/* <img src="" alt="" /> */}
+      </div>
     </div>
   )
 }
