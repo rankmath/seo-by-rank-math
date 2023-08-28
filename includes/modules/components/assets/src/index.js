21af775d1dcf6d3f870137208a662cc30aaa4ff2
Francis <numofran6@gmail.com>
2023-08-28 16:32:24 +0000

    button presentation reorganized and colors refactored

--- includes/modules/components/assets/src/index.js
+++ includes/modules/components/assets/src/index.js
@@ -28,11 +28,11 @@ function ButtonsShowcase() {
 			<h2>BUTTONS</h2>
 
 			<div>
-				<h3>PRIMARY</h3>
+				<h3>TEXT BUTTONS</h3>
 
 				<div className='components-wrapper'>
 					<div>
-						<h4>Text Buttons</h4>
+						<h4>Primary</h4>
 						<div className='button-group'>
 							<Button size='small'>Label</Button>
 
@@ -45,41 +45,7 @@ function ButtonsShowcase() {
 					</div>
 
 					<div className='margin-top'>
-						<h4>Icon + Text Buttons</h4>
-
-						<div className='button-group'>
-							<Button size='small' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button icon='rm-icon-rank-math'>Label</Button>
-
-							<Button size='large' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button size='large' icon='rm-icon-rank-math' disabled>Label</Button>
-						</div>
-					</div>
-
-					<div className='margin-top'>
-						<h4>Icon Buttons</h4>
-
-						<div className='button-group'>
-							<Button size='small' icon='rm-icon-rank-math' />
-
-							<Button icon='rm-icon-rank-math' />
-
-							<Button size='large' icon='rm-icon-rank-math' />
-
-							<Button size='large' icon='rm-icon-rank-math' disabled />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div>
-				<h3>PRIMARY OUTLINE</h3>
-
-				<div className='components-wrapper'>
-					<div>
-						<h4>Text Buttons</h4>
+						<h4>Primary Outline</h4>
 
 						<div className='button-group'>
 							<Button variant='primary-outline' size='small'>Label</Button>
@@ -93,41 +59,7 @@ function ButtonsShowcase() {
 					</div>
 
 					<div className='margin-top'>
-						<h4>Icon + Text Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='primary-outline' size='small' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='primary-outline' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
-						</div>
-					</div>
-
-					<div className='margin-top'>
-						<h4>Icon Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='primary-outline' size='small' icon='rm-icon-rank-math' />
-
-							<Button variant='primary-outline' icon='rm-icon-rank-math' />
-
-							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' />
-
-							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' disabled />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div>
-				<h3>SECONDARY</h3>
-
-				<div className='components-wrapper'>
-					<div>
-						<h4>Text Buttons</h4>
+						<h4>Secondary</h4>
 
 						<div className='button-group'>
 							<Button variant='secondary' size='small'>Label</Button>
@@ -141,41 +73,7 @@ function ButtonsShowcase() {
 					</div>
 
 					<div className='margin-top'>
-						<h4>Icon + Text Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='secondary' size='small' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='secondary' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='secondary' size='large' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='secondary' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
-						</div>
-					</div>
-
-					<div className='margin-top'>
-						<h4>Icon Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='secondary' size='small' icon='rm-icon-rank-math' />
-
-							<Button variant='secondary' icon='rm-icon-rank-math' />
-
-							<Button variant='secondary' size='large' icon='rm-icon-rank-math' />
-
-							<Button variant='secondary' size='large' icon='rm-icon-rank-math' disabled />
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div>
-				<h3>SECONDARY GREY</h3>
-
-				<div className='components-wrapper'>
-					<div>
-						<h4>Text Buttons</h4>
+						<h4>Secondary Grey</h4>
 
 						<div className='button-group'>
 							<Button variant='secondary-grey' size='small'>Label</Button>
@@ -189,7 +87,69 @@ function ButtonsShowcase() {
 					</div>
 
 					<div className='margin-top'>
-						<h4>Icon + Text Buttons</h4>
+						<h4>Tertiary Outline</h4>
+
+						<div className='button-group'>
+							<Button variant='tertiary-outline' size='small'>Label</Button>
+
+							<Button variant='tertiary-outline'>Label</Button>
+
+							<Button variant='tertiary-outline' size='large'>Label</Button>
+
+							<Button variant='tertiary-outline' size='large' disabled>Label</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<h3>ICON + TEXT BUTTONS</h3>
+
+				<div className='components-wrapper'>
+					<div>
+						<h4>Primary</h4>
+
+						<div className='button-group'>
+							<Button size='small' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button icon='rm-icon-rank-math'>Label</Button>
+
+							<Button size='large' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button size='large' icon='rm-icon-rank-math' disabled>Label</Button>
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Primary Outline</h4>
+
+						<div className='button-group'>
+							<Button variant='primary-outline' size='small' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='primary-outline' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Secondary</h4>
+
+						<div className='button-group'>
+							<Button variant='secondary' size='small' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='secondary' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='secondary' size='large' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='secondary' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Secondary Grey</h4>
 
 						<div className='button-group'>
 							<Button variant='secondary-grey' size='small' icon='rm-icon-rank-math'>Label</Button>
@@ -199,6 +159,68 @@ function ButtonsShowcase() {
 							<Button variant='secondary-grey' size='large' icon='rm-icon-rank-math'>Label</Button>
 
 							<Button variant='secondary-grey' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Tertiary Outline</h4>
+
+						<div className='button-group'>
+							<Button variant='tertiary-outline' size='small' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='tertiary-outline' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='tertiary-outline' size='large' icon='rm-icon-rank-math'>Label</Button>
+
+							<Button variant='tertiary-outline' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<h3>ICON BUTTONS</h3>
+
+				<div className='components-wrapper'>
+					<div>
+						<h4>Primary</h4>
+
+						<div className='button-group'>
+							<Button size='small' icon='rm-icon-rank-math' />
+
+							<Button icon='rm-icon-rank-math' />
+
+							<Button size='large' icon='rm-icon-rank-math' />
+
+							<Button size='large' icon='rm-icon-rank-math' disabled />
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Primary Outline</h4>
+
+						<div className='button-group'>
+							<Button variant='primary-outline' size='small' icon='rm-icon-rank-math' />
+
+							<Button variant='primary-outline' icon='rm-icon-rank-math' />
+
+							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' />
+
+							<Button variant='primary-outline' size='large' icon='rm-icon-rank-math' disabled />
+						</div>
+					</div>
+
+					<div className='margin-top'>
+						<h4>Secondary</h4>
+
+						<div className='button-group'>
+							<Button variant='secondary' size='small' icon='rm-icon-rank-math' />
+
+							<Button variant='secondary' icon='rm-icon-rank-math' />
+
+							<Button variant='secondary' size='large' icon='rm-icon-rank-math' />
+
+							<Button variant='secondary' size='large' icon='rm-icon-rank-math' disabled />
 						</div>
 					</div>
 
@@ -215,43 +237,9 @@ function ButtonsShowcase() {
 							<Button variant='secondary-grey' size='large' icon='rm-icon-rank-math' disabled />
 						</div>
 					</div>
-				</div>
-			</div>
-
-			<div>
-				<h3>TERTIARY OUTLINE</h3>
-
-				<div className='components-wrapper'>
-					<div>
-						<h4>Text Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='tertiary-outline' size='small'>Label</Button>
-
-							<Button variant='tertiary-outline'>Label</Button>
-
-							<Button variant='tertiary-outline' size='large'>Label</Button>
-
-							<Button variant='tertiary-outline' size='large' disabled>Label</Button>
-						</div>
-					</div>
 
 					<div className='margin-top'>
-						<h4>Icon + Text Buttons</h4>
-
-						<div className='button-group'>
-							<Button variant='tertiary-outline' size='small' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='tertiary-outline' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='tertiary-outline' size='large' icon='rm-icon-rank-math'>Label</Button>
-
-							<Button variant='tertiary-outline' size='large' icon='rm-icon-rank-math' disabled>Label</Button>
-						</div>
-					</div>
-
-					<div className='margin-top'>
-						<h4>Icon Buttons</h4>
+						<h4>Tertiary Outline</h4>
 
 						<div className='button-group'>
 							<Button variant='tertiary-outline' size='small' icon='rm-icon-rank-math' />
@@ -263,22 +251,18 @@ function ButtonsShowcase() {
 							<Button variant='tertiary-outline' size='large' icon='rm-icon-rank-math' disabled />
 						</div>
 					</div>
-				</div>
-			</div>
 
-			<div>
-				<h3>TERTIARY</h3>
+					<div className='margin-top'>
+						<h4>Tertiary</h4>
+						<div className='button-group'>
+							<Button variant='tertiary' icon='rm-icon-rank-math' size='small' />
 
-				<div className='components-wrapper'>
-					<h4>Icon Buttons</h4>
-					<div className='button-group'>
-						<Button variant='tertiary' icon='rm-icon-rank-math' size='small' />
+							<Button variant='tertiary' icon='rm-icon-rank-math' />
 
-						<Button variant='tertiary' icon='rm-icon-rank-math' />
+							<Button variant='tertiary' icon='rm-icon-rank-math' size='large' />
 
-						<Button variant='tertiary' icon='rm-icon-rank-math' size='large' />
-
-						<Button variant='tertiary' icon='rm-icon-rank-math' size='large' disabled />
+							<Button variant='tertiary' icon='rm-icon-rank-math' size='large' disabled />
+						</div>
 					</div>
 				</div>
 			</div>
