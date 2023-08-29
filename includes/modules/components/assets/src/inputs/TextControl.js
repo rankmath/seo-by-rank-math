7d07449a6119ef974f8e665d4c3dc6f219a0e43c
Francis <numofran6@gmail.com>
2023-08-29 16:31:50 +0000

    mixins created to refactor repeated css rules

--- includes/modules/components/assets/src/inputs/TextControl.js
+++ includes/modules/components/assets/src/inputs/TextControl.js
@@ -11,7 +11,7 @@ import '../../scss/text-control.scss';
 /**
  * WordPress dependencies
  */
-import { TextControl } from '@wordpress/components';
+import { TextControl, Disabled } from '@wordpress/components';
 import { useRef } from '@wordpress/element';
 
 export default ({
@@ -75,25 +75,26 @@ export default ({
 
 			{(isSuccess && !isError) &&
 				<div className='text-control-icon'>
-					<i className="rm-icon-tick validation-icon is-success"></i>
+					<i className="rm-icon-tick text-control-icon__validation is-success"></i>
 				</div>
 			}
+
 			{(isError && !isSuccess) &&
 				<div className='text-control-icon'>
-					<i className="rm-icon-trash validation-icon is-error"></i>
+					<i className="rm-icon-trash text-control-icon__validation is-error"></i>
 				</div>
 			}
 
 			{(type === 'number') && (
-				<div className='text-control-icon custom-number-controls'>
+				<div className='text-control-icon custom-number-control'>
 					<i
 						onClick={handleIncrement}
-						className={`rm-icon-plus control-icon ${disabled ? 'is-disabled' : ''}`}>
+						className={`rm-icon-plus custom-number-control__icon ${disabled ? 'is-disabled' : ''}`}>
 					</i>
 
 					<i
 						onClick={handleDecrement}
-						className={`rm-icon-trash control-icon ${disabled ? 'is-disabled' : ''}`}>
+						className={`rm-icon-trash custom-number-control__icon ${disabled ? 'is-disabled' : ''}`}>
 					</i>
 				</div>
 			)}
